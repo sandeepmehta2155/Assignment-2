d35870b8b067b94830861386c40754a8bcd931c1
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2022-08-28 01:39:54 -0400

    updated multiline comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 const l = console.log;
 
 // -------------------------------------------------------------------------------------------------
@@ -27,17 +27,18 @@ l(typeOfTriangle(34, 34, 35));
 l(typeOfTriangle(34, 42, 45));
 
 // -------------------------------------------------------------------------------------------------
-
-// 2. Write a function using switch case to find the grade of a student based
-// on marks obtained
-// a. “S grade” if the marks are between 90 and 100.
-// b. “A grade” if the marks are between 80 and 90.
-// c. “B grade” if the marks are between 70 and 80.
-// d. “C grade” if the marks are between 60 and 70.
-// e. “D grade” if the marks are between 50 and 60.
-// f. “E grade” if the marks are between 40 and 50.
-// g. “Student has failed” if the marks are between 0 and 40.
-// h. Else output “Invalid marks”.
+/*
+ 2. Write a function using switch case to find the grade of a student based
+ on marks obtained
+ a. “S grade” if the marks are between 90 and 100.
+ b. “A grade” if the marks are between 80 and 90.
+ c. “B grade” if the marks are between 70 and 80.
+ d. “C grade” if the marks are between 60 and 70.
+ e. “D grade” if the marks are between 50 and 60.
+ f. “E grade” if the marks are between 40 and 50.
+ g. “Student has failed” if the marks are between 0 and 40.
+ h. Else output “Invalid marks”.
+ */
 
 // Answer :
 // =>
@@ -100,22 +101,23 @@ const sum = (num = 1000) => {
 };
 
 // Output :
-l(sum())
-l(sum(50))
+l(sum());
+l(sum(50));
 
 // -------------------------------------------------------------------------------------------------
-
-// 4. Write a program to find the factorial of all prime numbers between a
-// given range . Range will be passed as 2 values in the function
-// parameters. eg- if it is needed to find the values for numbers 1-100, then
-// function declaration can look like - function prime(1,100).
+/*
+4. Write a program to find the factorial of all prime numbers between a
+given range . Range will be passed as 2 values in the function
+parameters. eg- if it is needed to find the values for numbers 1-100, then
+function declaration can look like - function prime(1,100).
+*/
 
 /**
- * 
- * @param {*} num 
+ *
+ * @param {*} num
  * @returns factorial of num
  */
- const factorialOfNum = (num) => (num !== 0 ? num * factorialOfNum(num - 1) : 1);
+const factorialOfNum = (num) => (num !== 0 ? num * factorialOfNum(num - 1) : 1);
 
 /**
  *
@@ -145,7 +147,7 @@ const prime = (numOne = 1, numTwo = 100) => {
 
     // flag = 1 means i is prime
     // and flag = 0 means i is not prime
-    if (flag === 1) l(i, " ",factorialOfNum(i));
+    if (flag === 1) l(i, " ", factorialOfNum(i));
   }
 };
 
